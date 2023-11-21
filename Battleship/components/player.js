502994b505912e844cd4382961acd3d9ce22785e
Shingfling <100726546+Chorwhat@users.noreply.github.com>
2023-11-20 23:44:21 -0500

    adds computer attack
    it recursively searches the board for a valid move, the test is to ensure it will find any place if there is one, even if not guessing smartly

--- Battleship/components/player.js
+++ Battleship/components/player.js
@@ -14,14 +14,27 @@ const Player = (() => {
             return enemy.recieveAttack(row,column)
         }
 
-        function computerAttack(enemy,row,column){
-            let enemyWidth = enemy.getWidth()
-            let enemyHeight = enemy.getHeight()
+        function computerAttack(enemy,row,column, depth=0){
+    
+            if (depth > 10) {
+                return false;
+            }
 
-            
+            if (enemy.isValidTarget(row,column)){
+                enemy.recieveAttack(row,column)
+                return true
+            }else{
+
+                return (
+                computerAttack(enemy,row+1,column, depth+1) ||
+                computerAttack(enemy,row-1,column, depth+1) ||
+                computerAttack(enemy,row,column+1, depth+1) ||
+                computerAttack(enemy,row,column-1, depth+1)
+                )
+            }
         }
 
-        return {getBoard, attack}
+        return {getBoard, attack, computerAttack}
     }
     return createPlayer
 })()
